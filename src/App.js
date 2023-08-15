77e3b9734b8bba8d3a31f37490068dce4ba033c2
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-08-15 17:59:48 +0300

    initial state setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [memberList, setmemberList] = useState([
+    "member1",
+    "member2",
+    "member3",
+  ]);
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +15,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {memberList.map((member) => (
+          <p>{member}</p>
+        ))}
       </header>
     </div>
   );
