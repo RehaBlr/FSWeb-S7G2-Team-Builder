3889747f63402be53332a2b9a5b07023c2f18173
Reha <134259610+RehaBlr@users.noreply.github.com>
2023-08-15 21:43:29 +0300

    state to obj

--- src/App.js
+++ src/App.js
@@ -4,19 +4,18 @@ import { useState } from "react";
 
 function App() {
   const [memberList, setmemberList] = useState([
-    "member1",
-    "member2",
-    "member3",
+    { name: "Ali", favTime: "Gece", focus: "Frontend" },
+    { name: "Veli", favTime: "Gündüz", focus: "Backend" },
+    { name: "Deli", favTime: "Gece", focus: "Backend" },
   ]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {memberList.map((member) => (
-          <p>{member}</p>
+        {memberList.map((member, ind) => (
+          <p key={ind}>
+            <strong>{member.name}</strong>:{member.favTime}-{member.focus}
+          </p>
         ))}
       </header>
     </div>
